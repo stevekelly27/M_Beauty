00e938fabba1b76583ee02cfda472d7b12cbc7cc
stevekelly27 <stevekelly28@hotmail.com>
2023-02-19 05:07:38 +0000

    added template for bag

--- bag/views.py
+++ bag/views.py
@@ -12,62 +12,62 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 
-def add_to_bag(request, product_id):
-    """ Add a quantity of the specified product to the shopping bag """
+# def add_to_bag(request, product_id):
+#     """ Add a quantity of the specified product to the shopping bag """
 
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    bag = request.session.get('bag', {})
+#     product = get_object_or_404(Product, pk=item_id)
+#     quantity = int(request.POST.get('quantity'))
+#     redirect_url = request.POST.get('redirect_url')
+#     size = None
+#     if 'product_size' in request.POST:
+#         size = request.POST['product_size']
+#     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[product_id]}')
-    else:
-        bag[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your bag')
+#     if item_id in list(bag.keys()):
+#         bag[item_id] += quantity
+#         messages.success(request, f'Updated {product.name} quantity to {bag[product_id]}')
+#     else:
+#         bag[item_id] = quantity
+#         messages.success(request, f'Added {product.name} to your bag')
 
-    request.session['bag'] = bag
-    return redirect(redirect_url)
+#     request.session['bag'] = bag
+#     return redirect(redirect_url)
     
 
-def adjust_bag(request, product_id):
-    """Adjust the quantity of the specified product to the specified amount"""
+# def adjust_bag(request, product_id):
+#     """Adjust the quantity of the specified product to the specified amount"""
 
-    product = get_object_or_404(Product, pk=product_id)
-    quantity = int(request.POST.get('quantity'))
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    bag = request.session.get('bag', {})
+#     product = get_object_or_404(Product, pk=product_id)
+#     quantity = int(request.POST.get('quantity'))
+#     size = None
+#     if 'product_size' in request.POST:
+#         size = request.POST['product_size']
+#     bag = request.session.get('bag', {})
 
-    if quantity > 0:
-        bag[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[product_id]}')
-    else:
-        bag.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your bag')
+#     if quantity > 0:
+#         bag[item_id] = quantity
+#         messages.success(request, f'Updated {product.name} quantity to {bag[product_id]}')
+#     else:
+#         bag.pop(item_id)
+#         messages.success(request, f'Removed {product.name} from your bag')
 
-    request.session['bag'] = bag
-    return redirect(reverse('view_bag'))
+#     request.session['bag'] = bag
+#     return redirect(reverse('view_bag'))
 
 
-def remove_from_bag(request, item_id):
-    """Remove the item from the shopping bag"""
+# def remove_from_bag(request, item_id):
+#     """Remove the item from the shopping bag"""
 
-    try:
-        product = get_object_or_404(Product, pk=product_id)
-        bag = request.session.get('bag', {})
+#     try:
+#         product = get_object_or_404(Product, pk=product_id)
+#         bag = request.session.get('bag', {})
 
-        bag.pop(product_id)
-        messages.success(request, f'Removed {product.name} from your bag')
+#         bag.pop(product_id)
+#         messages.success(request, f'Removed {product.name} from your bag')
 
-        request.session['bag'] = bag
-        return HttpResponse(status=200)
+#         request.session['bag'] = bag
+#         return HttpResponse(status=200)
 
-    except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)
+#     except Exception as e:
+#         messages.error(request, f'Error removing item: {e}')
+#         return HttpResponse(status=500)
