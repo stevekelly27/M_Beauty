a136359dc224d2dc0c25722a520b55e0e39976f6
stevekelly27 <stevekelly28@hotmail.com>
2023-03-16 12:11:36 +0000

    final deployment

--- m_beauty/settings.py
+++ m_beauty/settings.py
@@ -30,9 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
-DEBUG = True
 
 ALLOWED_HOSTS = ['m-beauty.herokuapp.com', 'localhost']
 
